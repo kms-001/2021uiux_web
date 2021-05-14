1ba8e3d438712b918739243eb238bc895134cfe1
kms <min9.9999u@gmail.com>
2021-05-14 12:17:02 +0900

    layout

--- 03theme/basic/head.php
+++ 03theme/basic/head.php
@@ -171,7 +171,9 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
 
 <!-- 콘텐츠 시작 { -->
 <div id="wrapper">
-    <div id="container_wr">
+    <div id="container_wr"> <!-- 1200px -->
 
-    <div id="container">
-        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
+
+
+    <div id="container" <? if(defined('_INDEX_')) {?> style="width:1200px"<?}?>> <!-- 930px -->
+        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }?>
